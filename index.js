d0e3743506df1dec9b00bfd40ad1d19c9b6f64c4
Danielle Huntrods <daniellehuntrods@gmail.com>
2017-01-23 15:47:34 +0000

    Add package info and ruleset

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    node: true,
+    es6: true,
+    mocha: true
+  },
+  rules: {
+    "indent": ["error", 2],
+    "func-names": "off",
+    "no-use-before-define": "off"
+  }
+};
