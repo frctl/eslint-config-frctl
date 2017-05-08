f4ee15d777da3941eea6744579e8ae8add968f42
Mark Perkins <mark@allmarkedup.com>
2017-05-08 11:39:34 +0100

    Allow unused vars in argument signatures

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ module.exports = {
   rules: {
     "indent": ["error", 2],
     "func-names": "off",
-    "no-use-before-define": "off"
+    "no-use-before-define": "off",
+    "no-unused-vars": ["error", { "args": "none" }]
   }
 };
